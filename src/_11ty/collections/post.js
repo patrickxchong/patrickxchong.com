a2626cb54c19ae1e3ef1775ad0f3eb804682b65b
Patrick Chong <patrickxchong@gmail.com>
2021-09-19 20:46:56 +0800

    feat: orders posts by both updated date and created date

--- src/_11ty/collections/post.js
+++ src/_11ty/collections/post.js
@@ -1,5 +1,10 @@
 module.exports = (collection) => {
-  let allPosts = [...collection.getFilteredByGlob('./src/posts/*.md')];
+  let allPosts = [...collection.getFilteredByGlob('./src/posts/*.md')].sort((a, b) => {
+    aDate = a.data.updatedAt || a.date;
+    bDate = b.data.updatedAt || b.date;
+    return bDate - aDate;
+
+  });
   if (process.env.NODE_ENV !== 'production')
     return allPosts;
   else
