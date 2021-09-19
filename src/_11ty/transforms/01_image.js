eb337af2a532f6e7daf5a78b420686077091dec6
Patrick Chong <patrickxchong@gmail.com>
2021-09-19 18:22:41 +0800

    feat: allow specifying desired sizes argument to picture element

--- src/_11ty/transforms/01_image.js
+++ src/_11ty/transforms/01_image.js
@@ -9,65 +9,71 @@ const path = require('path');
 const { parseHTML } = require('linkedom');
 
 module.exports = async (content, outputPath) => {
-  if (process.env.NODE_ENV === 'production'
-    && outputPath
-    && outputPath.endsWith('.html')) {
-    let { document } = parseHTML(content);
+  if (
+    process.env.NODE_ENV !== 'production'
+    || !outputPath
+    || !outputPath.endsWith('.html')) {
+    return content;
+  }
+  // process images from html files in production
 
-    const options = {
-      widths: [768, 1280, 1920],
-      // widths: [600, 900, 1500],
-      // widths: [25, 320, 640, 960, 1200, 1800, 2400],
-      formats: ["webp", "jpeg"],
-      urlPath: "/images/",
-      outputDir: "./dist/images/",
-      // human readable filename
-      filenameFormat: function (id, src, width, format, options) {
-        const extension = path.extname(src);
-        const name = path.basename(src, extension).replace(/[/\\?%*:|"<>]/g, '-'); // remove invalid filename characters
-        return `${name}-${width}w.${format}`;
-      }
-    };
+  let { document } = parseHTML(content);
 
-    let sizes = "(min-width: 768px) 33.3vw, 100vw"; // When screen > 768px, set width to 33.3vw, else it's 100vw 
-    // let sizes = "33.3vw";
-    // let sizes = "(min-width: 1024px) 1024px, 100vw";
-    // sizes="(max-width: 640px) 100%, 100%"
-    // sizes = "100vw"
+  const options = {
+    widths: [768, 1280, 1920],
+    // widths: [600, 900, 1500],
+    // widths: [25, 320, 640, 960, 1200, 1800, 2400],
+    formats: ["webp", "jpeg"],
+    urlPath: "/images/",
+    outputDir: "./dist/images/",
+    // human readable filename
+    filenameFormat: function (id, src, width, format, options) {
+      const extension = path.extname(src);
+      const name = path.basename(src, extension).replace(/[/\\?%*:|"<>]/g, '-'); // remove invalid filename characters
+      return `${name}-${width}w.${format}`;
+    }
+  };
 
-    const images = [...document.querySelectorAll('img')];
+  // When screen > 768px, set width to 33.3vw, else it's 100vw 
+  let defaultSizes = "(min-width: 768px) 33.3vw, 100vw";
+  // let sizes = "33.3vw";
+  // let sizes = "(min-width: 1024px) 1024px, 100vw";
+  // sizes="(max-width: 640px) 100%, 100%"
+  // sizes = "100vw"
 
-    await Promise.all(images.map(async (i, index) => {
+  const images = [...document.querySelectorAll('img')];
 
-      let src = i.getAttribute('src');
+  await Promise.all(images.map(async (i, index) => {
 
-      // regex to test if src is from an external URL -> "//"
-      // returns null if not external
-      if (!src.match(/.*\/\/.*/)) {
-        src = './src' + src; // append /src directory
-      }
+    let src = i.getAttribute('src');
+    let sizes = i.getAttribute('data-sizes') || defaultSizes;
 
-      const meta = await Image(src, options);
-      const last = meta.jpeg[meta.jpeg.length - 1];
-      if (last.width < 500) return;
+    // regex to test if src is from an external URL -> "//"
+    // returns null if not external
+    if (!src.match(/.*\/\/.*/)) {
+      src = './src' + src; // append /src directory
+    }
 
-      i.setAttribute('width', last.width);
-      i.setAttribute('height', last.height);
-      if (index !== 0) {
-        i.setAttribute('loading', 'lazy');
-        i.setAttribute('decoding', 'async');
-      }
+    const meta = await Image(src, options);
+    const last = meta.jpeg[meta.jpeg.length - 1];
+    if (last.width < 500) return;
 
-      // put webp before jpeg so that webp would be evaluated first
-      i.outerHTML = `
+    i.setAttribute('width', last.width);
+    i.setAttribute('height', last.height);
+    if (index !== 0) {
+      i.setAttribute('loading', 'lazy');
+      i.setAttribute('decoding', 'async');
+    }
+
+    // put webp before jpeg so that webp would be evaluated first
+    i.outerHTML = `
       <picture>
         <source type="image/webp" sizes="${sizes}" srcset="${meta.webp.map(p => p.srcset).join(', ')}">
         <source type="image/jpeg" sizes="${sizes}" srcset="${meta.jpeg.map(p => p.srcset).join(', ')}">
         ${i.outerHTML}
       </picture>`;
-    }));
+  }));
 
-    return `<!DOCTYPE html>${document.documentElement.outerHTML}`;
-  }
-  return content;
+  return `<!DOCTYPE html>${document.documentElement.outerHTML}`;
+
 };
--- src/_11ty/transforms/02_htmlmin.js
+++ src/_11ty/transforms/02_htmlmin.js
@@ -1,24 +1,23 @@
 const htmlmin = require('html-minifier');
 
 module.exports = (content, outputPath) => {
-  // returned minified content from html files in production
-  if (process.env.NODE_ENV === 'production'
-    && outputPath
-    && outputPath.endsWith('.html')) {
-
-    let minified = htmlmin.minify(content, {
-      useShortDoctype: true,
-      removeComments: true,
-      collapseWhitespace: true,
-      minifyCSS: true,
-      minifyJS: true,
-      collapseWhitespace: true,
-      conservativeCollapse: true
-    });
-
-    return minified;
-  } else {
-    // no transformation
+  if (
+    process.env.NODE_ENV !== 'production'
+    || !outputPath
+    || !outputPath.endsWith('.html')) {
     return content;
   }
+  // returned minified content from html files in production
+
+  let minified = htmlmin.minify(content, {
+    useShortDoctype: true,
+    removeComments: true,
+    collapseWhitespace: true,
+    minifyCSS: true,
+    minifyJS: true,
+    collapseWhitespace: true,
+    conservativeCollapse: true
+  });
+
+  return minified;
 };
