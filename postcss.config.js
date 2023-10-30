88c1d91df80ddb392c172ddc21c3a0500acd0a8e
Patrick Chong <patrickxchong@gmail.com>
2023-10-30 21:50:23 +0800

    fix: issue with postcss-scss plugin

--- postcss.config.js
+++ postcss.config.js
@@ -2,7 +2,6 @@ module.exports = {
   syntax: 'postcss-scss',
   plugins: [
     require('postcss-import'),
-    require('@mozaic-ds/postcss-sass'),
     require('tailwindcss'),
     ...process.env.NODE_ENV === "production" ? [require('autoprefixer'), require('cssnano')] : []
   ]
